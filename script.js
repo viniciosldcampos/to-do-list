95d6e6d2e78bc05bed6c74d7816e7819c783b1de
Vinicios <vinicios@vinicios.campos>
2025-11-07 09:52:10 -0300

    Cria a função addTask, variavel taskText - JS

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 const taskList = document.getElementById("taskList"); // taskList o elemento <ul> onde as tarefas aparecerão.
 const taskInput = document.getElementById("taskInput"); // taskInput o campo de texto onde o usuário digita a tarefa.
 
-// A função responsável por adicionar novas tarefas à lista.
+// Função responsável por adicionar novas tarefas à lista.
 function addTask() {
-    const taskText = taskInput.value.trim(); // Lê o texto digitado pelo usuário..value obtém o conteúdo do <input> e o .trim() remove espaços extras para evitar tarefas vazias como " ").
+    const taskText = taskInput.value.trim(); // Lê o texto digitado pelo usuário.,.value obtém o conteúdo do <input>. E .trim() remove espaços extras no início e no fim da string (para evitar tarefas vazias como " ").
+
+    // Verifica se o texto não está vazio. Se estiver vazio, nada acontece (impede que o usuário adicione uma linha em branco).
+    if(taskText !== "") {
+        const maxText = taskText.substring(0, 35); // Cria uma limitação de 35 primeiros caracteres da tarefa.
+        const li = document.createElement("li"); // Cria dinamicamente um novo elemento de lista (<li>) para inserir dentro do <ul>.
+    }
 }
